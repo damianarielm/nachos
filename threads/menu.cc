fbe0bdb8cb1c62b32dc66f59d2cab439c269b91f
damianarielm <drd00m@msn.com>
2020-02-20 15:03:29 -0300

    Agrego test de ports.

--- threads/menu.cc
+++ threads/menu.cc
@@ -3,6 +3,7 @@
 void ThreadTest();
 void Garden();
 void ProdCons();
+void PortTest();
 
 void Menu() {
     unsigned opt;
@@ -10,6 +11,7 @@ void Menu() {
     printf("0 - Thread test.\n");
     printf("1 - Ornamental garden.\n");
     printf("2 - Producer/consumer.\n");
+    printf("3 - Port test.\n");
     printf("Enter an option: ");
     scanf("%u", &opt);
 
@@ -17,6 +19,7 @@ void Menu() {
         case 0: ThreadTest(); break;
         case 1: Garden(); break;
         case 2: ProdCons(); break;
+        case 3: PortTest(); break;
         default: printf("Invalid option.\n");
     }
 }
---
+++ threads/port_test.cc
@@ -0,0 +1,70 @@
+#include <unistd.h>
+
+#include "system.hh"
+#include "synch.hh"
+
+unsigned nports;
+static unsigned time;
+Port** ports;
+
+void
+PortThread(void *name_) {
+    // Reinterpret arg `name` as a string.
+    char *name = (char *) name_;
+
+    while (1) {
+        usleep(time);
+        int port = Random() % nports;
+        int message = Random() % 999;
+
+        if (Random() % 2) {
+            printf("Thread `%s` trying to send %d trough port `%s`...\n" RESET,
+                    name, message, ports[port]->GetName());
+
+            ports[port]->Send(message);
+
+            printf("Thread `%s` successfully sended %d.\n" RESET,
+                    name, message);
+        } else {
+            printf("Thread `%s` triying to receive trough port `%s`...\n" RESET,
+                    name, ports[port]->GetName());
+
+            ports[port]->Receive(&message);
+
+            printf("Thread `%s` successfully received %d.\n" RESET, name, message);
+        }
+    }
+}
+
+/// Set up a ping-pong between several threads.
+///
+/// Do it by launching ten threads which call `SimpleThread`, and finally
+/// calling `SimpleThread` ourselves.
+void
+PortTest() {
+    unsigned threads;
+
+    printf("How many threads: ");
+    scanf("%d", &threads);
+
+    printf("How many ports: ");
+    scanf("%d", &nports);
+    ports = (Port**) malloc(sizeof(Port*) * nports);
+
+    printf("Time delay (ms): ");
+    scanf("%d", &time);
+    time *= 1000;
+
+    for (unsigned i = 0; i < nports; i++) {
+        char* name = new char[4];
+        sprintf(name, "%u", i);
+        ports[i] = new Port(name);
+    }
+
+    for (unsigned i = 1; i <= threads; i++) {
+        char *name = new char [4];
+        sprintf(name, "%u", i);
+        Thread *newThread = new Thread(name);
+        newThread->Fork(PortThread, (void *) name);
+    }
+}
