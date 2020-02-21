ab6fd8d324ae535a5babe27a72be1a59db948b07
damianarielm <drd00m@msn.com>
2020-02-21 02:03:13 -0300

    Agrego programa de usuario cp.

---
+++ userland/cp.c
@@ -0,0 +1,20 @@
+#include "syscall.h"
+#include "stdio.h"
+
+void main(int argc, char** argv) {
+    int src, dst; char c[1];
+    if (argc != 3)
+        print("Syntax error.\n");
+    else if ((src = Open(argv[1])) == -1) {
+        print("Can't open the file ");
+        print(argv[1]);
+        print(".\n");
+    } else {
+        Create(argv[2]);
+        dst = Open(argv[2]);
+        while (Read(c, 1, src)) Write(c, 1, dst);
+        Close(src); Close(dst);
+    }
+
+    Exit(0);
+}
